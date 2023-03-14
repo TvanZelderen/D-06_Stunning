d134d70dfc947bbd4ab5caa7f773c1fbc1f6e711
tzoli6 <zozoturi@gmail.com>
2023-03-14 09:31:54 +0100

    Revert "First file created"
    This reverts commit 7eeb2e356192e4952e53a1b69bcdd9942bd1de60.

--- load.py
+++ load.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 class Data:
 
-    def __init__(self):
+    def __init__(self, frame_no, stringer_no, weld_no, type):
+        self.frame_no = frame_no
+        self.stringer = 
+        self.frame = pd.read_csv('.\Frame_'+srt(), delimiter='\t', )
