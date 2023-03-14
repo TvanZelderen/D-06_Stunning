d4a30ca6944a4fb565b2e5876044b7b7a761a286
tvanzelderen <tvanzelderen@pm.me>
2023-03-14 10:22:16 +0100

    Added bar to N

--- load.py
+++ load.py
@@ -11,3 +11,17 @@ class Data:
         self.file_path = './STUNNING Demonstrator USW Data'+ folder + '/Frame_' + str(frame_no) + '/1kHz_' + str(stringer_no)+'_' + str(weld_no) + '.dat'
         self.frame = pd.read_csv(self.file_path, delimiter='\t', skiprows=[0], names=['Time_step', 'Pressure', 'Displacement'])
 
+    def normalize(self):
+        time_0 = self.frame.at[0, 'Time_step']
+        self.frame['Time_step'] = self.frame['Time_step'].sub(time_0)
+        self.frame['Time_step'] = self.frame['Time_step'].div(1000)
+        # print(self.frame['Time_step'][0:10])
+
+    def bar_to_N(self):
+        self.frame['Pressure'] = self.frame['Pressure'].mul(266.667)
+        print(self.frame['Pressure'][0:10])
+
+a = Data('01', '02', '01', 1)
+a.normalize()
+a.bar_to_N()
+
