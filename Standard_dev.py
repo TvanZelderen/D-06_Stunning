34639b6e7923f161718e75e77eeca8f1962997c6
tzoli6 <zozoturi@gmail.com>
2023-03-14 12:20:03 +0100

    Update Standard_dev.py

--- Standard_dev.py
+++ Standard_dev.py
@@ -1,4 +1,15 @@
 from load import Data
+import numpy as np
 
+frame_01 = np.array()
 
 for i in range(1,13):
+    if len(str(i)) == 1:
+        no = '0'+str(i)
+    else:
+        no = str(i)
+    frame_01.append(Data(frame_no='01', stringer_no=no, weld_no='01', type=1))
+    frame_01[i].normalize()
+    frame_01[i].bar_to_N()
+    frame_01[i].plot(axes=ax, displacement=False)
+    
