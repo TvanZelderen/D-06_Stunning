68415adcf2200a4abec66a85c563922b204b1cac
tzoli6 <zozoturi@gmail.com>
2023-03-14 09:45:08 +0100

    Update load.py

--- load.py
+++ load.py
@@ -4,5 +4,7 @@ class Data:
 
     def __init__(self, frame_no, stringer_no, weld_no, type):
         self.frame_no = frame_no
-        self.stringer = 
-        self.frame = pd.read_csv('.\Frame_'+srt(), delimiter='\t', )
+        self.stringer_no = stringer_no
+        self.weld_no = weld_no
+        self.type = type
+        self.frame = pd.read_csv('.\Frame_' + str(frame_no) + '\\1kHz_' + str(stringer_no)+'_' + str(weld_no) + '.dat', delimiter='\t', skiprows=[0], header=['Time_step', 'Pressure', 'Displacement'])
