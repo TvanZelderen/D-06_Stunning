0d8739457cf03f434ee291a475b0688c585555a3
LeslieOcean <80454346+LeslieOcean@users.noreply.github.com>
2023-03-16 17:00:48 +0100

    analysis

--- Analysis.py
+++ Analysis.py
@@ -7,8 +7,14 @@ a.normalize()
 a.bar_to_N()
 a.create_array()
 
-ax = plot_ini(title='yy')
-a.plot(axes=ax, displacement=False)
+b = Data('01', '02', '01', 1)
+b.normalize()
+b.bar_to_N()
+b.create_array()
+
+ax = plot_ini(title='power curve')
+a.plot(axes=ax, power=True)
+b.plot(axes=ax, power=True)
 plt.show()
 
 #print(a.frame.iloc[:,1].mean())
