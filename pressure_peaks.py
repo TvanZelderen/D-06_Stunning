da4ed6b6cf12ffa759c2b272dde9125d9eb55352
tzoli6 <zozoturi@gmail.com>
2023-03-16 15:23:37 +0100

    Idk what did I do

--- pressure_peaks.py
+++ pressure_peaks.py
@@ -1,7 +1,21 @@
 from load import Data as dt
+import numpy as np
+import matplotlib.pyplot as plt
 
 a = dt('01', '02', '02', 1)
 a.normalize()
 a.create_array()
 a.bar_to_N()
-print(a.array())
+p = a.frame['Pressure'].to_numpy()
+t = a.frame['Time'].to_numpy()
+avg = np.average(p)
+std = np.std(p)
+n = 2
+
+p_peak = p[np.where(p > (avg+n*std))]
+t_peak = t[np.where(p > (avg+n*std))]
+print(p_peak)
+plt.scatter(t_peak, p_peak)
+plt.plot(t, p)
+plt.show()
+
