4e9c5706b8da1acbea6ced0d2ba11b19ef85fa75
jfleur1 <jfleur14@gmail.com>
2023-03-16 17:11:57 +0100

    Peak values of curve

--- pressure_peaks.py
+++ pressure_peaks.py
@@ -16,8 +16,8 @@ p_peak = p[np.where(p > (avg+n*std))]
 t_peak = t[np.where(p > (avg+n*std))]
 
 plt.scatter(t_peak, p_peak)
-print(np.unique(p_peak))
-print(len(np.unique(p_peak)))
+print("The peak values are:", np.unique(p_peak))
+print("The number of peak values are:", len(np.unique(p_peak)))
 plt.plot(t, p)
 plt.show()
 
