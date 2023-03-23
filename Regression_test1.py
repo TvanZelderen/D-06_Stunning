f82dccb0a7b146a696ca0fb46987d228523133a3
LeslieOcean <80454346+LeslieOcean@users.noreply.github.com>
2023-03-23 14:02:56 +0100

    regression_test

--- Regression_test1.py
+++ Regression_test1.py
@@ -2,6 +2,8 @@ from load import *
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score
+
 
 '''Data systhesis'''   
 
@@ -23,8 +25,29 @@ def train_test_set(X, y, test_size, cv_size):
 
     return[X_train, X_test, X_cv, y_train, y_test, y_cv]
 
+'''Standardization'''
+def scale(X_train, X_test, X_cv):
+    scaler = StandardScaler().fit(X_train.T)
+
+    X_train1 = scaler.fit_transform(X_train.T)
+    X_test1 = scaler.transform(X_test.T)
+    X_cv1 = scaler.transform(X_cv.T)
+
+    return[X_train1, X_test1, X_cv1]
+
+
+'''Bestlearningrate'''
+def lrate():
+    
+    return lr_best
+
+
 '''Training'''
-def model(X, y, loss_function, ):
+def model(X, y, n_epochs, learning_rate = 1e-3, tot_tol = 1e-5):
+
+    
+
+
     return
     
 
@@ -32,10 +55,8 @@ total = iterate_points(type=0, frames=[1], stringers=[1,2])
 
 X, y = Data(total, num = 20)
 X_train, X_test, X_cv, y_train, y_test, y_cv = train_test_set(X, y, test_size=0.15, cv_size=0.1)
-scaler = StandardScaler().fit(X_train.T)
-X_scaled = scaler.fit_transform(X_train.T)
-y_scaled = scaler.fit_transform(y_train.T)
-print(X_scaled)
+
+print(X_train.index)
 
 
 #plt.scatter(np.linspace(0, 100, 20), total[3].frame['Power'].dropna().to_numpy()[0:20])
