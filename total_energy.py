7b3a1af3fdcb313a948481618d74bc36ce70b264
tzoli6 <zozoturi@gmail.com>
2023-03-23 16:04:58 +0100

    total_energy function done

--- total_energy.py
+++ total_energy.py
@@ -7,24 +7,17 @@ import numpy as np
 import seaborn as sns
 import pylab as py
 
-def total_energy(frame:list = [1], stringer:list = [1], weld:list = [1], type:int = 1):
+def total_energy(frame:list = [1], stringer:list = [2], weld:list = [1], type:int = 1):
     data = iterate_points(frames=frame, stringers=stringer, welds =weld, type=type)
     energy = []
     for i in data:
         p = i.frame['Power'].dropna().to_numpy()
         t = i.frame['Time'].drop(i.frame['Power'].isna()*range(len(i.frame['Power']))).to_numpy()
-        dt = t[1]-t[0]
-        print(len(t))
-        print(len(p))
-        energy.append(simpson(p, t))
-    print('Energy: ' + str(energy))
-    df_energy = pd.DataFrame(energy, columns=['energy'])
-    #plt.scatter(np.ones(len(energy)), energy)
-    sns.histplot(df_energy, x = 'energy')
-    plt.show()
+        energy.append([i.frame_no, i.stringer_no, i.weld_no, i.type, simpson(p, t)])
+    #print('Energy: ' + str(energy))
+    df_energy = pd.DataFrame(energy, columns=['Frame', 'Stringer', 'Weld', 'Type', 'Energy'])
+    return df_energy
+#plt.scatter(np.ones(len(energy)), energy)
+#sns.histplot(df_energy, x = 'Energy')
+plt.show()
 total_energy()
-#print(len(p))
-#print(len(t))
-#print(dt)
-
-print(energy)
