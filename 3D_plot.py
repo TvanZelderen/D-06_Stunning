2b11d10628d28c13386ffef65635c50142cf8dd3
tvanzelderen <tvanzelderen@pm.me>
2023-03-23 16:43:30 +0100

    Changed plotting, added logging

--- 3D_plot.py
+++ 3D_plot.py
@@ -37,7 +37,7 @@ for i in range(number_of_frames): # Plot the frame lines
     ax.plot(x, y, z, color='k', alpha=0.8) # actually plot, k = black, alpha = transparency
 
 for i in range(number_of_stringers): # Plot the stringer lines
-    theta = np.linspace(-np.pi*(10/180)-np.pi*(1-10/180)/number_of_stringers*i,-np.pi*(10/180)-np.pi*(1-10/180)/number_of_stringers*i, 50)
+    theta = np.linspace(-np.pi*(10/180)-np.pi*(1-10/180)/number_of_stringers*i,-np.pi*(10/180)-np.pi*(1-10/180)/number_of_stringers*i, 50) # I don't feel like doing actual trigonometry.
     x = fuselage_radius*np.cos(theta) + 0
     z = fuselage_radius*np.sin(theta) + fuselage_radius
     y = np.linspace(0, fuselage_length, 50)
@@ -45,10 +45,10 @@ for i in range(number_of_stringers): # Plot the stringer lines
 
 positions = [[1,1], [4,5]] # [stringer, frame]
 for position in positions:
-    theta = -np.pi*(10/180)-np.pi*(1-10/180)/number_of_stringers*position[0]
-    x = fuselage_radius*np.cos(theta) + 0
-    z = fuselage_radius*np.sin(theta) + fuselage_radius
-    y = side_offset + fuselage_length/number_of_frames*position[1]
+    theta = np.pi*(10/180)+np.pi*(160/180)/number_of_stringers*(position[0]-1) # Theta starts at 10 degrees and ends at 170 degrees.
+    x = - fuselage_radius*np.cos(theta) + 0 # The minus sign is because the cylinder is plotted in the negative x direction.
+    z = - fuselage_radius*np.sin(theta) + fuselage_radius # Starts at the middle of the cylinder and sweeps down.
+    y = side_offset + fuselage_length/number_of_frames*(position[1]-1) # Starts at the first frame (offset by -1).
     ax.scatter(x, y, z, color='r', marker='o', s=100) # Plot a point at the origin
 
 plt.show()
