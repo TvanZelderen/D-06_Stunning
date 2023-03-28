c04e0d212b5a4e0a0a95ec8149a2863d64ee406f
RiesvanHeur <ries_vanheur@live.nl>
2023-03-28 09:58:59 +0200

    histplot

--- energy_analysis.py
+++ energy_analysis.py
@@ -3,6 +3,6 @@ import seaborn as sns
 import numpy as np
 from total_energy import total_energy as tot
 
-a = tot([1],[2, 3],[1],1)
-plt.hist(a)
-plt.show
+a = tot([1],[2, 3],[4],1)
+sns.histplot(a)
+plt.show()
