db9546c3ef1a740ae7723868cd545a75730a874c
RiesvanHeur <ries_vanheur@live.nl>
2023-03-28 11:04:01 +0200

    -

--- energy_analysis.py
+++ energy_analysis.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import numpy as np
 from total_energy import energy 
 
-a = energy([1],[2, 3, 4, 5],[1],1)
+a = energy([1],[2, 3, 4, 5, 6],[1],1)
 print(a)
 sns.histplot(a["Energy"])
 plt.show()
