d9cea82aa4625a2a63642c9bb3761f574622aa34
bernardoaves <118188264+bernardoaves@users.noreply.github.com>
2023-03-28 10:23:21 +0200

    displacement and other stuff :egg:

--- comparisons.py
+++ comparisons.py
@@ -1,7 +1,7 @@
 from load import *
 
 ax = plot_ini('test')    
-i = Data(1,2,2,1)
-print(i.frame)
-i.plot(ax, force=True)
+atotal = iterate_points(type='All')
+for i in atotal:
+    i.plot(ax, displacement=True)
 plot_legends()
