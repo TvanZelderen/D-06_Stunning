24d58216e790029ad83b429b322956ade1144bc4
RiesvanHeur <ries_vanheur@live.nl>
2023-03-28 11:54:29 +0200

    -

--- energy_analysis.py
+++ energy_analysis.py
@@ -4,15 +4,15 @@ import numpy as np
 from total_energy import energy 
 
 
-a = energy([1],list(range(2,29)),[1],2)
+a = energy([1],list(range(2,29)),[1],1)
 b= np.mean(a["Energy"])
 print("The mean is:", b)
 
 for i in range(2,29):
     c = energy([1],[i],[1],2)
-    print(c["Energy"])
-    #if c["Energy"] < 0.5*b:
-        #print(c)
+    #print(c["Energy"])
+    if c["Energy"] < 0.5*b:
+        print(c)
 
 sns.histplot(a["Energy"])
 plt.show()
