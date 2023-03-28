6e6da08648f2f41ce5b4a645203461bd6e09f596
bernardoaves <118188264+bernardoaves@users.noreply.github.com>
2023-03-28 09:38:59 +0200

    import logging

--- comparisons.py
+++ comparisons.py
@@ -1,9 +1,7 @@
 from load import *
 
 ax = plot_ini('test')    
-atotal = iterate_points(frames=[10])
-print(atotal)
-
-# i.smoothing()
-# i.plot(ax, smooth_power=True)
-# plot_legends()
+i = Data(1,2,2,1)
+print(i.frame)
+i.plot(ax, force=True)
+plot_legends()
