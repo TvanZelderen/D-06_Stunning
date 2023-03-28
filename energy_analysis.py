410222fac82b0d0aace859eceaa8fa70683b2730
RiesvanHeur <ries_vanheur@live.nl>
2023-03-28 10:25:59 +0200

    help

--- energy_analysis.py
+++ energy_analysis.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-from total_energy import energy as tot
+from total_energy import energy 
 
-a = tot([1],[2, 3],[1],1)
+a = energy([1],[2, 3, 4, 5],[1],1)
 print(a)
-sns.histplot(a)
+sns.histplot(a["Energy"])
 plt.show()
