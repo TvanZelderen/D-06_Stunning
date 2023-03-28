82fd17e8c80e78abecf3b9ac3b0a6fffc51339c5
Blackbirdie72 <f.w.a.sprangers@student.tudelft.nl>
2023-03-28 10:17:17 +0200

    .

--- DisplacmentAnalysis.py
+++ DisplacmentAnalysis.py
@@ -8,16 +8,19 @@ import seaborn as sns
 import pylab as py
 
 
-data = iterate_points(frames=[10])
+data = iterate_points(frames=[12])
 
-print(data[1])
+d = []
+kAlarm = []
+iAlarm = []
+# t = []
+# print(data)
 
-
-# energy = []
-# for i in data:
-#     d = i.frame['Displacement'].dropna().to_numpy()
-#     t = i.frame['Time'].drop(i.frame['Displacement'].isna()*range(len(i.frame['Displacement']))).to_numpy()
-#     dt = t[1]-t[0]
+for i in data:
+    d.append(i.frame['Displacement'].dropna().to_numpy())
+    t = (i.frame['Time'].drop(i.frame['Displacement'].isna()*range(len(i.frame['Displacement']))).to_numpy())
+    dt = t[1]-t[0]
+    
 
 # d = data.frame['Displacement']
 # na_rows = d.isna()
@@ -27,17 +30,34 @@ print(data[1])
 # t.drop(rows_remove, axis=0)
 # t.reset_index()
 
-# print('Energy: ' + str(energy))
-# df_energy = pd.DataFrame(energy, columns=['energy'])
+# print(len(d))
 
-# i = 1
+# print('Displacement: ' + len(d))
+# Displacement = pd.DataFrame(Displacement, columns=['Displacement'])
+# print(d)
 
-# while i != len(d) :
-#     if d[i-1] > d[i] :
-#         print("FFFFF", i)
+i = 1
+k = 0
 
-#     i = i + 1
+while k != len(d) :
+    while i != len(d[k]) :
+        if d[k][i-1] > d[k][i] :
+            print("Positive slope Alarm! i = ", i,"k = ", k)
+            iAlarm.append(i)
+            kAlarm.append(k)
+        i = i + 1
+    
+    k = k + 1
 
 
-# plt.plot(d)
+q = 0
+while q != len(kAlarm) :
+    plt.plot(d[kAlarm[q]])
+    print(len(kAlarm))
+    plt.show()
+    q = q + 1
+
+
+
+# plt.plot(d[1])
 # plt.show()
