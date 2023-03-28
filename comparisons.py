22514dee850fa0774e6fc7146117f4702c82f493
bernardoaves <118188264+bernardoaves@users.noreply.github.com>
2023-03-28 11:30:42 +0200

    regular update

--- comparisons.py
+++ comparisons.py
@@ -1,7 +1,7 @@
 from load import *
-
+from shape import *
 ax = plot_ini('test')    
-atotal = iterate_points(type='All')
+atotal = iterate_points(type=1)
 for i in atotal:
     i.plot(ax, displacement=True)
 plot_legends()
