340a12946cb4e429fe93a31735a5065a4dd4a786
RiesvanHeur <ries_vanheur@live.nl>
2023-03-30 14:30:01 +0200

    -

--- energy_analysis.py
+++ energy_analysis.py
@@ -4,11 +4,11 @@ import numpy as np
 from total_energy import energy 
 
 
-a = energy([1],list(range(2,29)),[1],1)
+a = energy([1],list(range(0,29)),[1],1)
 b= np.mean(a["Energy"])
 print("The mean is:", b)
 
-for i in range(2,29):
+for i in range(0,29):
     c = energy([1],[i],[1],2)
     #print(c["Energy"])
     if c["Energy"] < 0.5*b:
