c2d8fabd8f031be3b13a04929b9b9ae8d107b2f6
bernardoaves <118188264+bernardoaves@users.noreply.github.com>
2023-04-25 17:30:05 +0200

    uhhh

--- shape.py
+++ shape.py
@@ -31,6 +31,9 @@ def get_peaks(obj, power_norm=False, time_norm=False):
     power_2 = np.gradient(power_1,time)  #second dev
 
     roots = root_finder(power_1,time)
+    if len(roots) == 0:
+        obj.highest_peak = False
+        return []
     for t in roots:
         value = np.interp(t, time, power)
         second_dev = np.interp(t, time, power_2)
@@ -43,6 +46,8 @@ def get_peaks(obj, power_norm=False, time_norm=False):
             t /= tot_t
             second_dev *= tot_t
         peaks_valleys.append((t,value,second_dev))
+    peaks_valleys.sort(key = lambda x: x[1])
+    obj.highest_peak = peaks_valleys[-1]
     return peaks_valleys
 
 def mean_curve(obj_list):
@@ -73,11 +78,21 @@ def square_diff(x_axis, y_mean, obj):
     obj.diff = sum(loc_diff)
     return obj.diff
 
+def peak_metric(x_axis, y_mean, obj):
+    if obj.highest_peak == False:
+        obj.peak_m = 0
+    else:
+        t = obj.highest_peak[0]
+        y_peak = obj.highest_peak[1]
+        y_curve = np.interp(t,x_axis,y_mean)
+        obj.peak_m = (y_peak-y_curve)/t
+    
+    return obj.peak_m
+
 def rank(obj):
     index = ranked_diff.index(obj.diff)
     ranking = index+1
     return ranking
-
 
 if __name__ == '__main__':
 
@@ -92,23 +107,53 @@ if __name__ == '__main__':
         #     i.plot(ax, norm_power=True)
     
     x_axis, y_mean = mean_curve(all_obj)
-    #plt.plot(x_axis, y_mean, linewidth=3, color='black')
-    #plot_legends()
+
+    ssds = []
+    pm = []
+    x_plot = []
+    y_plot = []
 
     for i in all_obj:
-        square_diff(x_axis, y_mean, i)
-        if i.diff < 0:
-            i.plot(ax, norm_power=True)
+        if 'Power' not in i.frame.keys() or len(i.frame['Power'].dropna())==0:
+            continue
+        else:
+            i.smoothing()
+            get_peaks(i, time_norm=True, power_norm=True)
+            ssds.append(square_diff(x_axis, y_mean, i))
+            pm.append(peak_metric(x_axis, y_mean, i))
+            x_plot.append(i.frame_no + ((i.weld_no-1)%3)/10 - 0.10)
+            y_plot.append(i.stringer_no + ((i.weld_no-1)//3)/2.5 - 0.20)
+
+        # if i.diff < 0:
+        #     i.plot(ax, norm_power=True)
     #plt.plot(x_axis, y_mean, linewidth=3, color='black')
    # plot_legends()
 
-    diff_list = [obj.diff for obj in all_obj]
+    x_plot = np.array(x_plot)
+    y_plot = np.array(y_plot)
 
-    ranked_obj = sorted(all_obj, key=lambda x: x.diff)
-    ranked_diff = sorted(diff_list)
+    idx = np.argsort(ssds)
+    sorted_x_plot = x_plot[idx]
+    sorted_y_plot = y_plot[idx]
+    ssd_rank = np.arange(1,len(ssds)+1)
+    plt.scatter(sorted_x_plot, sorted_y_plot, c=ssd_rank, cmap='coolwarm')
+    plt.colorbar()
+    plt.show()
 
-    for i in range(0,10):
-        print(all_obj[i].main_label + ': Rank ' + str(rank(all_obj[i])) + ' out of ' + str(len(all_obj)))
+    # good_ones = [i for i in all_obj if i.diff<9 and i.peak_m>0.53]
+    # ax = plot_ini('test') 
+    # for i in good_ones:
+    #     i.plot(ax, norm_power=True)
+    # plt.plot(x_axis, y_mean, linewidth=3, color='black')
+    # plot_legends()
+
+    # diff_list = [obj.diff for obj in all_obj]
+
+    # ranked_obj = sorted(all_obj, key=lambda x: x.diff)
+    # ranked_diff = sorted(diff_list)
+
+    # for i in range(0,10):
+    #     print(all_obj[i].main_label + ': Rank ' + str(rank(all_obj[i])) + ' out of ' + str(len(all_obj)))
 
 
     # plt.hist(diff_list,bins=30)
@@ -121,7 +166,7 @@ if __name__ == '__main__':
     #         continue
     #     else:
     #         obj.smoothing()
-    #         peaks = get_peaks(obj, time_norm=True, power_norm=True)
+    #         peaks = get_peaks(obj, time_norm=True, power_norm=True, first_only=True)
     #         all_peaks += peaks
 
     # all_peaks = [x for x in all_peaks if x[2]<0]
