e9ef69d3386b051d8b589fa4c3664f255c6d74af
tzoli6 <zozoturi@gmail.com>
2023-04-25 17:27:47 +0200

    Update load.cpython-39.pyc

--- energy_2D_plot.py
+++ energy_2D_plot.py
@@ -1,20 +1,37 @@
 from load import Data
 from total_energy import energy
 from matplotlib import pyplot as plt
+from matplotlib import patches
 from matplotlib.gridspec import GridSpec
 import numpy as np
+axs = []
+fig, axs = plt.subplots(2, 6, subplot_kw=dict(projection="polar"))
+axs = np.reshape(axs, -1)
+ax_iter = iter(axs)
+print(axs)
+for j in range(0, 12):
+    theta = np.array([], float)
+    radii = np.array([], float)
+    for i in range(0, 21):
+        dtheta = np.array([9*i])#np.linspace(9*i-4.5, 9*i+4.5, 1)
+        try:
+            en = energy(stringer=[i+1], frame = [j+1], type=1)
+            dradii = np.ones(1)*float(en['Energy'])
+        except:
+            dradii = np.zeros(1)
+        theta = np.concatenate((theta, dtheta), axis = 0)
+        radii = np.concatenate((radii, dradii), axis = 0)
 
-theta = np.array([0])
-radii = np.array([0])
-for i in range(0, 20):
-    dtheta = np.linspace(9*i-4.5, 9*i+4.5, 10)
-    try:
-        dradii = np.ones(10)*float(energy(stringer=[i+1])['Energy'])
-    except:
-        dradii = np.zeros(10)
-    print(dtheta)
-    print(dradii)
-    theta = np.concatenate(theta, dtheta)
-    radii = np.concatenate(radii, dradii)
 
-print(theta)
+    theta = -theta*np.pi/180
+    radii = radii/1000
+    print(theta)
+    print(radii)
+    ax = next(ax_iter)
+    ax.bar(theta, radii, bottom = 0.0, width = 9*np.pi/180)
+    radii[radii == 0] = np.nan
+    ax.plot(np.linspace(0, 2*np.pi, 50), np.ones(50)*np.nanmean(radii), color = 'red')
+    #ax.xaxis.set_ticks(range(0, 360, 9))
+    ax.yaxis.set_ticks(range(1,4, 1))
+    ax.set_title('Frame '+str(j+1))
+plt.show()
