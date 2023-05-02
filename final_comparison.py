52b2e27fcc17845df1916cde899c81750ba5ac7b
tvanzelderen <tvanzelderen@pm.me>
2023-05-02 15:17:55 +0200

    final comparison code in progress

--- final_comparison.py
+++ final_comparison.py
@@ -1,4 +1,4 @@
-import csv
+import pandas as pd
 
 # Weights for the final comparison of the models
 w_displacement = 1
@@ -7,23 +7,19 @@ w_power = 2
 
 
 # Suspect welds in displacement
-suspectwelds = [[1,2,2,3], [1,4,2,3], [2,3,3,5]]
+suspectwelds = [1223, 1423, 2335]
 
-# Write the data to CSV file
-def write_to_csv(suspectwelds, data_type='displacement'):
-    with open('final_comparison.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Weldnumber', 'Displacement', 'Pressure', 'Power'])
-        for weld in suspectwelds:
-            clip_type = weld[0]
-            frame = weld[1]
-            stringer = weld[2]
-            weld = weld[3]
-            if data_type == 'displacement':
-                writer.writerow([weld, 1, 0, 0])
-            elif data_type == 'pressure':
-                writer.writerow([weld, 0, 1, 0])
-            elif data_type == 'power':
-                writer.writerow([weld, 0, 0, 1])
+# Open the csv with pandas and create a dataframe
+df = pd.read_csv('final_comparison.csv', header=0, names=['Weld', 'Displacement', 'Pressure', 'Power'])
 
-write_to_csv(suspectwelds)
+# Find the if the suspect welds are in the dataframe
+for weld in suspectwelds:
+    # Index in the dataframe
+    index = df.index[(df['Weld'] == weld)]
+    print(index)
+    # If the index is not empty write to second column w_displacement
+    if not index.empty:
+        df.iloc[index[0], 1] = w_displacement
+        print(df.iloc[index[0], 1])
+
+                     
