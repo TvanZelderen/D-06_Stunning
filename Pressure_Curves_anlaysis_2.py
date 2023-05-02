eeabeb66674dd5881554a6db72dcd20a02e29d18
Miguel Güell <miguegy@gmail.com>
2023-05-02 17:30:21 +0200

    h

--- Pressure_Curves_anlaysis_2.py
+++ Pressure_Curves_anlaysis_2.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
 from load import Data as dt
 from load import iterate_points
 #from Pressure_Curves_analysis import peakvalues 
 from math import isnan
+import seaborn as sns
+sns.set_theme()
 
 a = dt('01', '02', '02', 1) #choose (frame_no, stringer_no, weld_no, type)
 b = dt('04', '12', '01', 1) #choose (frame_no, stringer_no, weld_no, type)
@@ -77,15 +78,18 @@ def normalize_peaks(peaksp,peakst, avg):
             h=0
             m=0
             loc=0
+            repeat =False
+            #print(peakst)
             while h<n:
                 pressure_dif=abs(peaksp[i-n+h]-avg)
                 if(m<pressure_dif):
                     m = pressure_dif
                     loc=i-n+h
                 h=h+1
+           
             tn.append(peakst[loc])
             pn.append(peaksp[loc])
-            print(tn, pn, peakst[loc], loc, i, n, h)
+            #print(tn, pn, peakst[loc], loc, i, n, h)
             """ pn.append(max(selected_values))
             print(selected_values)
             print(max(selected_values))
@@ -97,68 +101,120 @@ def normalize_peaks(peaksp,peakst, avg):
             n=n+1
             
         i=i+1
-    
-    return tn, pn
+
+        tn_unique = []
+        pn_unique = []
+        for t, p in zip(tn, pn):
+            if t not in tn_unique:
+                tn_unique.append(t)
+                pn_unique.append(p)
+
+    return tn_unique, pn_unique
 
 def av_freq(tn, pn):
     i=0
     t=[]
+    idx = np.argsort(tn)
+    tn = np.array(tn)[idx]
+    pn = np.array(pn)[idx]
+
     while i<(len(tn)-1):
         t.append(tn[i+1]-tn[i])
         i=i+1
-    m = max(t)
-
+    ma = max(t)
+    mi = min(t)
     
     
     tav=np.average(t)
     pav=np.average(pn)
-    return tav, pav, m
+    return tav, pav, ma, mi
 
 
-objects = iterate_points(type=1, stringers= [2])
-print(objects)
+objects = iterate_points(type=1)
+print(len(objects))
 number=[]
 i=1
+weld_list = []
 for obj in objects:
     p = obj.frame['Pressure'].to_numpy()
     t = obj.frame['Time'].to_numpy()
     peaksp,peakst, avg =calculatepeack(obj)
     max_time_freq , max_pressure_freq = normalize_peaks(peaksp,peakst, avg)
-    print(max_time_freq)
-    av_time, av_pressure, m = av_freq(max_time_freq, max_pressure_freq)
+    #print(max_time_freq)
+    av_time, av_pressure, ma, mi = av_freq(max_time_freq, max_pressure_freq)
     number.append(i)
-    print(m, av_time)
-    plt.figure()
-    plt.plot(peakst, peaksp, "o")
-    plt.plot(max_time_freq, max_pressure_freq, "o")
-    plt.plot(t, p)
-    plt.xlabel("time")
-    plt.ylabel("pressure")
-    plt.show()
+    print(ma, mi, av_time)
+
+    weld_location = [obj.frame_no, obj.stringer_no, obj.weld_no, obj.type]
+    
+    """if i==13:
+        plt.figure()
+        plt.plot(peakst, peaksp, "o")
+        plt.plot(max_time_freq, max_pressure_freq, "o")
+        print(max_pressure_freq)
+        print(max_time_freq)
+        plt.plot(t, p)
+        plt.xlabel("time")
+        plt.ylabel("pressure")
+        plt.show()"""
+
+    weld_list.append([weld_location, ma, mi, av_time])#creates a matrix with the weld location, maximum and minimum time difference and average time
     i=i+1
 
-"""plt.figure()
-plt.subplot(111)
-plt.plot(number, av_time)
-plt.xlabel("frames")
-plt.ylabel("average time")
-plt.show()"""
+averages_averages = [lst[3] for lst in weld_list]
+total_average_avarage = np.average(averages_averages)
+averages_minimums = [lst[2] for lst in weld_list]
+total_average_minimums = np.average(averages_minimums)
+averages_maximums = [lst[1] for lst in weld_list]
+total_average_maximumx = np.average(averages_maximums)
+std_avarges=np.std(averages_averages)
+std_minimums=np.std(averages_minimums)
+std_maximums=np.std(averages_maximums)
+
+value=0.5
+bad_weld_avg_avg=[]
+bad_weld_avg_max=[]
+bad_weld_avg_min=[]
+col=0
+color=[]
+for num in range(len(averages_averages)):
+    
+    if averages_averages[num]> total_average_avarage+value*std_avarges:
+        bad_weld_avg_avg.append(weld_list[num][0])
+        col=col+1
+
+    if averages_minimums[num]< total_average_minimums+value*std_minimums:
+        bad_weld_avg_min.append(weld_list[num][0])
+        col=col+1
+
+    if averages_maximums[num]> total_average_maximumx+value*std_maximums:
+        bad_weld_avg_max.append(weld_list[num][0])
+        col=col+1
+    color.append([weld_list[num], col])
+    
+    col=0
+#print(bad_weld_avg_avg)
+print(color)
+print(color[0])
+print(color[0][1])
 
 
-p = a.frame['Pressure'].to_numpy()
-t = a.frame['Time'].to_numpy()
-peaksp, peakst, avg=calculatepeack(a)
-max_time_freq, max_pressure_freq=normalize_peaks(peaksp, peakst, avg)
-#for i in range (len(peaksp)):
-#   print(peakst[i], peaksp[i])
-"""plt.plot(peakst, peaksp, "o")
-plt.plot(max_time_freq, max_pressure_freq, "o")
-plt.plot(t, p)
-plt.show()"""
-#print("hi")
-for i in range(len(max_time_freq)):
-    print(max_time_freq[i], max_pressure_freq[i])
-print()
-print(max_time_freq[1])
-print(max_time_freq[0])
-print(av_freq(max_time_freq, max_pressure_freq))
+color_plot = []
+x_plot = []
+y_plot = []
+for weld in color:
+    x_plot.append(weld[0][0][0] + ((weld[0][0][2]-1)%3)/10 - 0.10)
+    y_plot.append(weld[0][0][1] + ((weld[0][0][2]-1)//3)/2.5 - 0.20)
+    color_plot.append(weld[1])
+plt.scatter(x_plot, y_plot, c=color_plot, cmap='coolwarm')
+plt.colorbar()
+plt.show()
+
+for weld in bad_weld_avg_avg:
+    x_plot.append(weld[0][0] + ((weld[0][2]-1)%3)/10 - 0.10)
+    y_plot.append(weld[0][1] + ((weld[0][2]-1)//3)/2.5 - 0.20)
+    color_plot.append(weld[1])
+plt.scatter(x_plot, y_plot, c=color_plot, cmap='coolwarm')
+plt.colorbar()
+plt.show()
+
