b446d7097d91f857bce74d19f3124272e8d5966b
tvanzelderen <tvanzelderen@pm.me>
2023-05-02 15:50:56 +0200

    changed to dict

--- final_comparison.py
+++ final_comparison.py
@@ -7,19 +7,40 @@ w_power = 2
 
 
 # Suspect welds in displacement
-suspectwelds = [1223, 1423, 2335]
+suspectwelds = [[1,2,2,3], [1,4,2,3], [2,3,3,5]]
+suspectwelds_2 = [[1,1,2,3], [1,4,3,6], [2,1,1,1]]
+suspectwelds_3 = [[1,1,1,1]]
 
 # Open the csv with pandas and create a dataframe
 df = pd.read_csv('final_comparison.csv', header=0, names=['Weld', 'Displacement', 'Pressure', 'Power'])
 
-# Find the if the suspect welds are in the dataframe
-for weld in suspectwelds:
-    # Index in the dataframe
-    index = df.index[(df['Weld'] == weld)]
-    print(index)
-    # If the index is not empty write to second column w_displacement
-    if not index.empty:
-        df.iloc[index[0], 1] = w_displacement
-        print(df.iloc[index[0], 1])
+# Write suspect position welds to a txt file
+with open('suspectwelds_position.txt', 'w') as f:
+    print(f'Opened {f.name} for writing')
+    for i in suspectwelds:
+        f.write(str(i)+'\n')
 
-                     
+# Write suspect pressure welds to a txt file
+with open('suspectwelds_pressure.txt', 'w') as f:
+    print(f'Opened {f.name} for writing')
+    for i in suspectwelds_2:
+        f.write(str(i)+'\n')
+
+# Write suspect power welds to a txt file
+with open('suspectwelds_power.txt', 'w') as f:
+    print(f'Opened {f.name} for writing')
+    for i in suspectwelds_3:
+        f.write(str(i)+'\n')
+
+# Pull the welds from text files into a dictionary
+final_weld_comparison = {}
+with open('suspectwelds_position.txt', 'r') as f:
+    for line in f.readlines():
+        final_weld_comparison[str(line)] = w_displacement
+with open('suspectwelds_pressure.txt', 'r') as f:
+    for line in f.readlines():
+        final_weld_comparison[str(line)] = w_pressure
+with open('suspectwelds_power.txt', 'r') as f:
+    for line in f.readlines():
+        final_weld_comparison[str(line)] = w_power
+print(final_weld_comparison)
