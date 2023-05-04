d95558c3426421d6f97127e1c8f5cceb4c8062d6
LeslieOcean <80454346+LeslieOcean@users.noreply.github.com>
2023-05-04 15:50:28 +0200

    improve the structure

--- Dataspecific.py
+++ Dataspecific.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from load import *
 from sklearn.preprocessing import Normalizer, StandardScaler
 import seaborn as sns
-from Autoencoder import trainning
+from Autoencoder import LOF
+from collections import defaultdict
 
 
 def Data(total):
@@ -23,6 +24,23 @@ def Data(total):
         X.fillna(0, inplace=True)
     return X.T
 
+def counter(abnomal_skin, b):
+     counts = defaultdict(int)
+     list_of_suspicious = []
+     list_of_baddy = []
+
+     for result in abnomal_skin:
+          counts[result] += 1
+
+     for result, count in counts.items():
+          if count == b-2 or b-1:
+               list_of_suspicious.append(result)
+          if count == b:
+               list_of_baddy.append(result)
+     
+     
+     return list_of_suspicious, list_of_baddy
+
 '''Data processing'''
 #clip_to_skin
 total0 = iterate_points(type = 0)
@@ -30,9 +48,9 @@ X0 = Data(total0)
 #clip_to_frame
 total1 = iterate_points(type = 1)
 X1 = Data(total1)
-#print(X)
+#print(X1)
 
-'''Data Normalization'''
+     #Data Normalization
 scaler_0 = StandardScaler()
 scaler_1 = StandardScaler()
 normalized_features_0 = scaler_0.fit_transform(X0)
@@ -45,12 +63,16 @@ normalized_features1 = scaler1.fit_transform(normalized_features_1)
 normalized_data1 = pd.DataFrame(normalized_features1, columns = X1.columns)
 #print(normalized_data0)
 
-abnomal_skin ,abnomal_frame = trainning(3, X0, X1, total0, total1, normalized_data0, normalized_data1)
+a = 5
+abnomal_skin, abnomal_frame, X_0, X_scores_0, X_1, X_scores_1, outlier_indice_0, outlier_indice_1 = LOF(a, X0, X1, total0, total1, normalized_data0, normalized_data1)
 
-print(abnomal_skin)
+'''List of ill-welding'''
+list_of_suspicious_skin, list_of_baddy_skin = counter(abnomal_skin, a)
+list_of_suspicious_frame, list_of_baddy_frame = counter( abnomal_frame, a)
+print(list_of_baddy_skin, list_of_baddy_frame, list_of_suspicious_skin, list_of_suspicious_frame)
 
 
-'''Visualization
+'''Visualization'''
 
 fig, (ax0, ax1) = plt.subplots(1, 2)
 
@@ -81,13 +103,9 @@ ax1.scatter(
 ax1.legend(loc="upper left")
 ax1.title.set_text("Local Outlier Factor (LOF) for clip-to-frame")
 plt.show()
-'''
-'''Map'''
-'''for i in range(len(outlier_indice_0)):
-     print(total0[outlier_indice_0[i]].main_label)
-for j in range(len(outlier_indice_1)):
-     print(total1[outlier_indice_1[j]].main_label)
 
+'''Map'''
+fig, (ax_0, ax_1) = plt.subplots(1, 2)
 
 x_plot = []
 y_plot = []
@@ -95,8 +113,6 @@ y_plot = []
 for i in total0:
      x_plot.append(i.frame_no + ((i.weld_no-1)%3)/10 - 0.10)
      y_plot.append(i.stringer_no + ((i.weld_no-1)//3)/2.5 - 0.20)
-
-
 x_plot = np.array(x_plot)
 y_plot = np.array(y_plot)
 
@@ -104,9 +120,25 @@ idx = np.argsort(radius_0)
 sorted_x_plot = x_plot[idx]
 sorted_y_plot = y_plot[idx]
 ssd_rank = np.arange(1,len(X_scores_0)+1)
-plt.scatter(sorted_x_plot, sorted_y_plot, c=radius_0, cmap='Oranges')
-plt.colorbar()
-plt.show()'''
-''''''
+im1 = ax_0.scatter(sorted_x_plot, sorted_y_plot, c=radius_0, cmap='Oranges')
+fig.colorbar(im1, ax = ax_0)
+
+x_plot = []
+y_plot = []
+
+for i in total1:
+     x_plot.append(i.frame_no + ((i.weld_no-1)%3)/10 - 0.10)
+     y_plot.append(i.stringer_no + ((i.weld_no-1)//3)/2.5 - 0.20)
+x_plot = np.array(x_plot)
+y_plot = np.array(y_plot)
+
+idx = np.argsort(radius_1)
+sorted_x_plot = x_plot[idx]
+sorted_y_plot = y_plot[idx]
+ssd_rank = np.arange(1,len(X_scores_1)+1)
+im2 = ax_1.scatter(sorted_x_plot, sorted_y_plot, c=radius_1, cmap='Blues')
+fig.colorbar(im2, ax = ax_1)
+plt.show()
+
 
 
