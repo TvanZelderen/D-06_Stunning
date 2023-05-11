e45338e99521f5fd46f81aec7a2f3b33a432550d
tzoli6 <zozoturi@gmail.com>
2023-05-11 15:34:24 +0200

    h

--- energy_2D_plot.py
+++ energy_2D_plot.py
@@ -4,18 +4,20 @@ from matplotlib import pyplot as plt
 from matplotlib import patches
 from matplotlib.gridspec import GridSpec
 import numpy as np
+import seaborn as sns
+sns.set_theme()
 axs = []
 fig, axs = plt.subplots(3, 4, subplot_kw=dict(projection="polar"))
 axs = np.reshape(axs, -1)
 ax_iter = iter(axs)
-weld_loc = 1
+weld_loc = 2
 weld_type = 1
 print(axs)
 for j in range(0, 12):
     theta = np.array([], float)
     radii = np.array([], float)
     for i in range(0, 31):
-        dtheta = np.array([9*i])#np.linspace(9*i-4.5, 9*i+4.5, 1)
+        dtheta = np.array([6*i])#np.linspace(9*i-4.5, 9*i+4.5, 1)
         try:
             en = energy(stringer=[i+1], frame = [j+1], type=weld_type, weld=[weld_loc])
             dradii = np.ones(1)*float(en['Energy'])
@@ -32,17 +34,17 @@ for j in range(0, 12):
     ax = next(ax_iter)
     #ax.set_ylim([1, max(radii)])
 
-    ax.xaxis.set_ticks(np.arange(0, -189, -13)*np.pi/180) #was np.arange(0, -189, -18)
-    ax.set_xticklabels(['1', '3', '5', '7', '9', '11', '13', '15', '17', '19', '21', '23', '25', '27','29']) 
+    ax.xaxis.set_ticks(np.arange(0, -190, -18)*np.pi/180) #was np.arange(0, -189, -18)
+    ax.set_xticklabels(['1', '4', '7', '10', '13', '16', '19', '21', '24', '27', '30'])# ['1', '3', '5', '7', '9', '11', '13', '15', '17', '19', '21', '23', '25', '27','29', '30']) 
     ax.yaxis.set_ticks([1, 2, 3, 4])
 
-    ax.bar(theta, radii, bottom = 0.0, width = 9*np.pi/180, label = 'Weld Enegry [kJ]')
+    ax.bar(theta, radii, bottom = 0.0, width = 6*np.pi/180, label = 'Weld Enegry [kJ]')
     radii[radii == 0] = np.nan
     ax.plot(np.linspace(0, 2*np.pi, 50), np.ones(50)*np.nanmean(radii), color = 'green', linewidth= 0.7 , label = 'Mean Weld Enegry [kJ]')
     
 
     #ax.legend()
-    ax.set_title('Frame '+str(j+1) + '\n Mean: ' + str(np.round(np.nanmean(radii), 1)) + 'kJ' + ' STD: ' +str(np.round(np.nanstd(radii), 2)))
+    ax.set_title('Frame '+str(j+1) + '\n \u03BC = ' + str(np.round(np.nanmean(radii), 1)) + 'kJ' + ' \u03C3 = ' +str(np.round(np.nanstd(radii), 2)))
     ax.set_thetamin(4.5)
     ax.set_thetamax(-184.5)
 handles, labels = ax.get_legend_handles_labels()
