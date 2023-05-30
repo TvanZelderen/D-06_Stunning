5866763638b46b806bed5040b1231f08c4d4a583
bernardoaves <118188264+bernardoaves@users.noreply.github.com>
2023-05-30 16:43:03 +0200

    moving around shit

--- gen_plots.py
+++ gen_plots.py
@@ -15,10 +15,10 @@ newcolors = cmap(np.linspace(cmin, cmax, 256))
 newcmp = ListedColormap(newcolors)
 
 #structure = [filename, type(0/1), title, data label]
-file_list = [['DisplacementPositiveClipToFrame.csv',1,''],
-             ['DisplacementPositiveClipToSkin.csv',0,''],
-             ['DisplacementVeryHighWeldsClipToFrame.csv',1,''],
-             ['DisplacementVeryHighWeldsClipToSkin.csv',0,'']]
+file_list = [['DisplacementPositiveClipToFrame.csv',1,'Score [-]'],
+             ['DisplacementPositiveClipToSkin.csv',0,'Score [-]'],
+             ['DisplacementVeryHighWeldsClipToFrame.csv',1,'Score [-]'],
+             ['DisplacementVeryHighWeldsClipToSkin.csv',0,'Score [-]']]
 
 for file_idx in file_list:
     with open(file_idx[0], 'r', newline='') as file:
@@ -30,6 +30,9 @@ for file_idx in file_list:
         row[:] = row[0]+[row[1]]
     data = np.array(data)
     data = data.astype('float64')
+
+    if file_idx[1] == 1:
+        data = data[data[:,2]<=2]
 
     frame_no = data[:,0].reshape(-1)
     stringer_no = data[:,1].reshape(-1)
@@ -56,7 +59,7 @@ for file_idx in file_list:
     plt.scatter(x_plot, y_plot, c=data_plot, cmap=newcmp, s=10, clim=(0,10))
     cbar= plt.colorbar()
     cbar.set_label(file_idx[2], rotation=90)
-    plt.xlabel('Frame number')
-    plt.ylabel('Stringer number')
+    plt.xlabel('Frame number [-]')
+    plt.ylabel('Stringer number [-]')
 
     plt.show()
